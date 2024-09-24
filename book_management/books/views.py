e7d74b2d9cbccc2abb72b6d8996ce5f7c3c5d695
Galeta Mikhail <mihailiy05@icloud.com>
2024-09-24 14:29:37 +0300

    Paths, models added

--- book_management/books/views.py
+++ book_management/books/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# books/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
